37fe7b27cc5709e02cca5c9fde8f13596ebfca07
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-07 18:39:02 -0300

    Adicionando a classe ParkingSpotModel e seus atributos

---
+++ src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
@@ -0,0 +1,48 @@
+package com.api.parkingcontrol.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "parking_spot_tb")
+public class ParkingSpotModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 18)
+    private String parkingSpotNumber;
+
+    @Column(nullable = false, unique = true, length = 7)
+    private String licensePlateCar;
+
+    @Column(nullable = false, length = 70)
+    private String brandCar;
+
+    @Column(nullable = false, length = 70)
+    private String modelCar;
+    
+    @Column(nullable = false)
+    private LocalDateTime registrionDate;
+    
+    @Column(nullable = false, length = 130)
+    private String responsibleName;
+    
+    @Column(nullable = false, length = 30)
+    private String apartment;
+
+    @Column(nullable = false, length = 30)
+    private String block;
+}
