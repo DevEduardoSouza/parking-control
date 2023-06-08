84cb3760ffc13d89041bf82349716cdf422f0906
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-08 10:54:29 -0300

    Adicionando um metódo Get

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -1,8 +1,23 @@
 package com.api.parkingcontrol.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.parkingcontrol.services.ParkingSpotService;
+
 @RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/parking-spot")
 public class ParkingSpotController {
-    
+
+    @Autowired
+    private ParkingSpotService parkingSpotService;
+
+    @GetMapping("/listar")
+    public String listar() {
+        return "Olá mundo";
+    }
 }
