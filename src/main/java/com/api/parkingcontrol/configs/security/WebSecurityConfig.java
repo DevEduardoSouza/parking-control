38cca5a2276a6c5a51f65b8b498306526c2b4668
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-12 17:24:22 -0300

    Adicionando o WebSecurityConfig

---
+++ src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.api.parkingcontrol.configs.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class WebSecurityConfig {
+
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity)throws Exception{
+        httpSecurity
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+
+        return httpSecurity.build();
+    }
+    
+}
